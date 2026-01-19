c9fe45c330f43a0818ffbfc1670086b823c18b7a
Taiqin Peng <taiqin.peng@stud.uni-heidelberg.de>
2026-01-20 03:19:56 +0800

    assignment10

--- de/seuhd/observer/ConcreteObserver.java
+++ de/seuhd/observer/ConcreteObserver.java
@@ -2,17 +2,30 @@ package de.seuhd.observer;
 
 public class ConcreteObserver implements Observer {
     // TODO: Add fields and static variables.
+    private static int nextId = 1;
+
+    private final int id;
+    private final Subject subject;
 
     public ConcreteObserver(Subject subject) {
         // TODO: Implement constructor.
+        this.subject = subject;
+        this.id = nextId++;
+        subject.attach(this);
     }
 
     @Override
     public void update(Subject subject) {
         // TODO: Implement update method.
+        if (subject instanceof ConcreteSubject) {
+            ConcreteSubject concreteSubject = (ConcreteSubject) subject;
+            int value = concreteSubject.getValue();
+            System.out.println("Observer " + id + " received update from subject : New value is " + value);
+        }
     }
 
     public void detach() {
         // TODO: Implement detach method.
+        subject.detach(this);
     }
 }
